ab8acded0c55bdf489ff52b3457c6c96f94d98d7
FixPng <897491068@qq.com>
2023-02-12 23:53:13 +0800

    v2.0 gin

--- gin_study/视图/3.Restful风格.go
+++ gin_study/视图/3.Restful风格.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -16,8 +17,24 @@ type Response struct {
 	Msg  string `json:"msg"`
 }
 
-// 文章列表
+// 封装一个解析json到结构体上的函数
+func _bindJson(c *gin.Context, obj any) (err error) {
+	body, _ := c.GetRawData()
+	contentType := c.GetHeader("Content-Type")
+	switch contentType {
+	case "application/json":
+		err := json.Unmarshal(body, &obj)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+// 文章列表页面
 func _getList(c *gin.Context) {
+	// 包含搜索，分页
 	articleList := []ArticleModel{
 		{"Go语言入门", "这边文章是《Go语言入门》"},
 		{"python语言入门", "这边文章是《python语言入门》"},
@@ -26,6 +43,7 @@ func _getList(c *gin.Context) {
 	c.JSON(200, Response{0, articleList, "成功"})
 }
 
+// 文章详情
 func _getDetail(c *gin.Context) {
 	// 获取param中的id
 	fmt.Println(c.Param("id"))
@@ -37,14 +55,36 @@ func _getDetail(c *gin.Context) {
 
 // 创建文章
 func _create(c *gin.Context) {
+	// 接收前端传递来的JSON数据
+	var article ArticleModel
 
-	c.JSON(200, gin.H{})
+	err := _bindJson(c, &article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(200, Response{0, article, "成功"})
 }
+
+// 编辑文章
 func _update(c *gin.Context) {
-	c.JSON(200, gin.H{})
+	fmt.Println(c.Param("id"))
+	var article ArticleModel
+
+	err := _bindJson(c, &article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(200, Response{0, article, "修改成功"})
 }
+
+// 删除文章
 func _delete(c *gin.Context) {
-	c.JSON(200, gin.H{})
+	fmt.Println(c.Param("id"))
+	c.JSON(200, Response{0, map[string]string{}, "删除成功"})
 }
 
 func main() {
---
+++ gin_study/视图/4.请求头相关.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+func main() {
+	router := gin.Default()
+
+	// 请求头的各种获取方式
+	router.GET("/", func(c *gin.Context) {
+		// 首字母大小写不区分 单词与单词之间用 - 连接
+		// 用于获取一个请求头
+		fmt.Println(c.GetHeader("Accept-Encoding"))
+
+		// Header 是一个不同的 map[string][]string
+		fmt.Println(c.Request.Header)
+		// 如果是使用 Get方法或者是.GetHeader,那么可以不用区分大小写，并且返回第一个value
+		fmt.Println(c.Request.Header.Get("Accept-Encoding"))
+		// 如果使用map的方式取值，请注意大小写问题
+		fmt.Println(c.Request.Header["Accept-eNcoding"])
+
+		c.JSON(200, gin.H{"msg": "成功"})
+	})
+
+	// 爬虫和用户的区别对待
+	router.GET("/index", func(c *gin.Context) {
+		userAgent := c.GetHeader("User-Agent")
+		// 用正则去匹配
+		// 字符串的包含匹配
+		if strings.Contains(userAgent, "python") {
+			//爬虫来了
+			c.JSON(0, gin.H{"data": "爬虫来了！！"})
+			return
+		}
+
+		c.JSON(0, gin.H{"data": "识别为用户"})
+
+	})
+
+	router.Run(":8080")
+
+}
