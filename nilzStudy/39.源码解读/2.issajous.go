38cc56aee55a0fdeea221a9277094e095d41a83e
FixPng <8978491068@qq.com>
2023-02-23 17:13:03 +0800

    v2.0 gin

--- nilzStudy/39.源码解读/2.issajous.go
+++ nilzStudy/39.源码解读/2.issajous.go
@@ -21,7 +21,7 @@ const (
 
 func main() {
 	// The sequence of images is deterministic unless we seed
-	// the pseudo-random number generator using the current time.
+	// the pseudo-random number generator using the current timego.
 	// Thanks to Randall McPherson for pointing out the omission.
 	rand.Seed(time.Now().UTC().UnixNano())
 	lissajous(os.Stdout)
